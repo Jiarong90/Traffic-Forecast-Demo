7c00c0dc5164f461dad97b6e5348a6f3bee42fb1
Jiarong90 <34557678+Jiarong90@users.noreply.github.com>
2026-01-11 18:13:38 +0800

    Demo

---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("LTA_API_KEY")
+if not API_KEY:
+    raise Exception("API Key not found")
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def root():
+    return FileResponse("static/index.html")
+
+@app.get("/api/incidents")
+def get_incidents():
+    url_incidents = "https://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents"
+    headers = {
+        "AccountKey": API_KEY,
+        "accept": "application/json"
+    }
+    num_incidents = 10
+
+    data = requests.get(url_incidents, headers=headers).json()
+    incidents = data.get("value", [])
+    return {"incidents": incidents[:num_incidents]}
